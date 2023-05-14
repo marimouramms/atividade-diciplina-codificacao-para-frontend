1f586d5c572b666a55ad2029c3b3e05545ec5340
Mari Moura <mari.mouramms@gmail.com>
2023-05-13 21:13:06 -0300

    Adicionando o Bootstrap, o Jquery e criando a index e pagina de login da loja Game Mania, atividade da diciplina de Codificação Front End do Senai.

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//efeito do botão voltar ao Topo
+function topo() {
+    window.scrollTo(
+        {
+            top: 0,
+            left: 0,
+            behavior: 'smooth',
+        }
+
+    )
+
+}
+//Validação de Login
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario == 'admin' && senha == '1234') {
+        window.location = "index.html";
+        logado = 1;
+
+    }
+
+    if (logado == 0) {
+        alert("Acesso Negado. Dados Incorretos!")
+    }
+}
+
+function cadastro() {
+    alert("Cadastro feito com sucesso")
+    window.location.href = "index.html";
+
+
+}
+//Ativar alert no botão cadastrar
